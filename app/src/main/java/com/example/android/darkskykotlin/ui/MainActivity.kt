d402d83b80a34f3c9060598a82f3c14b3815fbef
slinny <siranli411@gmail.com>
2020-03-24 22:57:01 -0400

    added weather fragment

--- app/src/main/java/com/example/android/darkskykotlin/ui/MainActivity.kt
+++ app/src/main/java/com/example/android/darkskykotlin/ui/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.darkskykotlin.R
 import com.example.android.darkskykotlin.adapter.DailyAdapter
@@ -14,37 +16,15 @@ import com.example.android.darkskykotlin.util.WeatherIcons
 import com.example.android.darkskykotlin.viewmodel.WeatherViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-const val REQUEST_COARSE_LOCATION = 5678
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: WeatherViewModel
-
-    private var weatherIconMap: Map<String, Drawable>? = null
-
-    private val adapter = DailyAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
-
-        addObservers()
-
-        weatherIconMap = WeatherIcons.map(this)
-
-        viewModel.getUsersCurrentLocation()
-
-        daily_recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        daily_recyclerview.adapter = adapter
-    }
-
-    private fun addObservers() {
-
-        viewModel.weatherApiResponseLiveData.observe(this, Observer { weatherModel ->
-
-            adapter.setDayForecast(weatherModel.daily.data)
-        })
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+        actionBar?.setTitle("DarkSkyWeather")
     }
 }
---
+++ app/src/main/java/com/example/android/darkskykotlin/ui/WeatherFragment.kt
@@ -0,0 +1,61 @@
+package com.example.android.darkskykotlin.ui
+
+import android.app.ActionBar
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android.darkskykotlin.R
+import com.example.android.darkskykotlin.adapter.DailyAdapter
+import com.example.android.darkskykotlin.databinding.FragmentWeatherBinding
+import com.example.android.darkskykotlin.util.WeatherIcons
+import com.example.android.darkskykotlin.viewmodel.WeatherViewModel
+import kotlinx.android.synthetic.main.fragment_weather.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class WeatherFragment : Fragment() {
+
+    private lateinit var viewModel: WeatherViewModel
+
+    private var weatherIconMap: Map<String, Drawable>? = null
+
+    private val adapter = DailyAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentWeatherBinding>(
+            inflater,
+            R.layout.fragment_weather, container, false
+        )
+        viewModel = ViewModelProviders.of(this).get(WeatherViewModel::class.java)
+
+        viewModel.weatherApiResponseLiveData.observe(this, Observer { weatherModel ->
+
+            adapter.setDayForecast(weatherModel.daily.data)
+        })
+
+        weatherIconMap = WeatherIcons.map(this.context!!)
+
+        viewModel.getUsersCurrentLocation()
+
+        binding.dailyRecyclerview.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        binding.dailyRecyclerview.adapter = adapter
+        (activity as AppCompatActivity).supportActionBar?.title = "DarkSkyWeather"
+
+        return binding.root
+    }
+
+}
